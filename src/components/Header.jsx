2e33c929fe8f6a97a08f64474e8298ef58b58a79
Uddict <uditkp024@gmail.com>
2024-09-18 11:30:00 +0530

    cmt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
   return (
     <div className="bg-indigo-300 shadow-sm text-[#172048] py-3 text-center text-[26px] font-semibold">
       Dictation HealthCare
+      <button>login</button>
     </div>
   );
 };
